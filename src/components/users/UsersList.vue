9b9ca7d56865d7c00cdd281f302de1e59f35e896
butters5789 <butters5789@gmail.com>
2022-08-11 14:38:17 -0600

    before route leave

--- src/components/users/UsersList.vue
+++ src/components/users/UsersList.vue
@@ -1,4 +1,6 @@
 <template>
+  <button @click="saveChanges">Save Changes</button>
+
   <ul>
     <user-item
       v-for="user in users"
@@ -17,9 +19,29 @@ export default {
     UserItem,
   },
   inject: ['users'],
+  data() {
+    return {
+      changesSaved: false,
+    };
+  },
+  methods: {
+    saveChanges() {
+      this.changesSaved = true;
+    },
+  },
   beforeRouteEnter(to, from, next) {
     console.log('Before Route Enter', to, from);
     next();
+  },
+  beforeRouteLeave(to, from, next) {
+    console.log('Before Route Leave', to, from);
+
+    if (this.changesSaved) {
+      next(true);
+    } else {
+      const verify = confirm('Are you sure you want to leave this page?');
+      next(verify);
+    }
   },
 };
 </script>
